dd17a6c1c5f4e9de2bdaa2e8ff6ce6afb2bf5a10
alkaitagi <alkaitagi@outlook.com>
2021-06-03 21:13:05 +0300

    Add uploading dialog blck

--- lib/phrases/UploadButton.dart
+++ lib/phrases/UploadButton.dart
@@ -19,39 +19,57 @@ class UploadButton extends StatefulWidget {
 }
 
 class _UploadButtonState extends State<UploadButton> {
-  var _loading = false;
-  final storage = FirebaseStorage.instance;
-
   Future<void> upload() async {
-    setState(() {
-      _loading = true;
-    });
     final files = widget.phrases.map((p) => File(p.path)).toList();
     final zipFile = File('${widget.directory.path}/all.zip');
     await ZipFile.createFromFiles(
-        sourceDir: widget.directory, files: files, zipFile: zipFile);
-    await storage.ref('uploads/recordings.zip').putFile(zipFile);
+      sourceDir: widget.directory,
+      files: files,
+      zipFile: zipFile,
+    );
+    await FirebaseStorage.instance
+        .ref('uploads/recordings_new.zip')
+        .putFile(zipFile);
     await zipFile.delete();
     await Future.wait(files.map((file) => file.delete()));
     ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Content uploaded successfully!')));
-    setState(() {
-      _loading = false;
-    });
+      SnackBar(content: Text('Content uploaded successfully!')),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return _loading
-        ? Container(
-            width: 24,
-            height: 24,
-            child: CircularProgressIndicator(),
-          )
-        : IconButton(
-            icon: Icon(Icons.cloud_upload_outlined),
-            onPressed: widget.phrases.isEmpty ? null : upload,
-            tooltip: 'Upload',
-          );
+    return IconButton(
+      icon: Icon(Icons.cloud_upload_outlined),
+      onPressed: widget.phrases.isEmpty
+          ? null
+          : () => showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) {
+                  upload().then((_) => Navigator.pop(context));
+                  return WillPopScope(
+                    onWillPop: () async => false,
+                    child: AlertDialog(
+                      contentPadding: const EdgeInsets.all(0),
+                      content: Container(
+                        height: 128,
+                        width: 128,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            SizedBox(height: 16),
+                            Text('Uploading, please wait...'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+      tooltip: 'Upload',
+    );
   }
 }

30295b17d4edfa8d77b20e949782d093fbfecace
alkaitagi <alkaitagi@outlook.com>
2021-06-08 20:36:58 +0300

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:phrase_recorder/chapters/chapters_list.dart';
-import 'package:phrase_recorder/phrases/phrase_list.dart';
 import 'package:phrase_recorder/store.dart';
 
 void main() {
--- lib/phrases/phrase_recorder.dart
+++ lib/phrases/phrase_recorder.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_sound/flutter_sound.dart';

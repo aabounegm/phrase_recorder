65ba0f5d070da57a8baeb5800dcb776ae23f50a5
alkaitagi <alkaitagi@outlook.com>
2021-06-25 08:25:50 +0300

    Add scenario serialization UI

--- lib/scenario/scenario_page.dart
+++ lib/scenario/scenario_page.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:phrase_recorder/scenario/exercises/exercise_builder.dart';
 import 'node/node_card.dart';
 import 'scenario.dart';
+import 'package:flutter/services.dart';
 
 class ScenarioPage extends StatefulWidget {
   final Scenario scenario;
@@ -24,11 +26,70 @@ class _ScenarioPageState extends State<ScenarioPage> {
     );
   }
 
+  Future<void> serializeScenario() async {
+    final textController = TextEditingController();
+    textController.text = jsonEncode(scenario.toJson());
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Select assignment'),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 8,
+              ),
+              child: TextField(
+                controller: textController,
+                readOnly: true,
+                maxLines: null,
+                decoration: InputDecoration.collapsed(
+                  hintText: '',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 8,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Clipboard.setData(
+                        ClipboardData(text: textController.text),
+                      );
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.content_copy_outlined),
+                    label: Text('COPY TO CLIPBOARD'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Exercises'),
+        actions: [
+          IconButton(
+            onPressed: serializeScenario,
+            icon: Icon(Icons.code_outlined),
+            tooltip: 'Scenario to JSON',
+          ),
+          SizedBox(width: 4),
+        ],
       ),
       body: ListView(
         children: [
